978aeb292c4b9e4b899dbb78f529a543167ba8e7
Erik Egeland Steinnes <erik.1@hotmail.no>
2022-10-18 20:47:39 +0200

    updates with While loop

--- wordscoring.py
+++ wordscoring.py
@@ -2,10 +2,14 @@ import string
 values = dict()
 for index, letter in enumerate(string.ascii_lowercase):
    values[letter] = index + 1
-   
-total = 0
-word = input("what word do you want to know the score of?")
-for letter in word.lower():
-    if letter in values:
-        total += values[letter]
-print(total, "% for the word ", word)
+
+while True:
+    total = 0
+    word = input("what word do you want to know the score of?")
+    if word == "0":
+        break
+    else:
+        for letter in word.lower():
+            if letter in values:
+                total += values[letter]
+        print(total, "% for the word ", word)
